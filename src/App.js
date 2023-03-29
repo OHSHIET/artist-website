6d044b53fac1c7a3a0230aaddd00996ca7a111e3
Лев Леицкий <leitskiy13@mail.ru>
2023-03-29 23:51:08 +0200

    fix gh pages

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import FirstPage from "./pages/FirstPage";
 import SecondPage from "./pages/SecondPage";
 import NoPage from "./pages/NoPage";
+import Navbar from './pages/Navbar';
+
+const mainPath = "/artist-website";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="App">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="artist-website" element={<Home />} />
-          <Route path="page1" element={<FirstPage />} />
-          <Route path="page2" element={<SecondPage />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/artist-website/page1" element={<FirstPage />} />
+        <Route path="/artist-website/page2" element={<SecondPage />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
-
-      {/* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is first page.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */}
 }
 
 export default App;
